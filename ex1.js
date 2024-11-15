ab6f9f6dc350fa6942954ada0573d25a5bd09e51
Gabriell-Marcondes-Moro <gabrielmarcomdesmoro3@gmail.com>
2024-11-14 23:24:22 -0300

    Add files via upload

--- ex1.js
+++ ex1.js
@@ -1,7 +1,8 @@
-//  Imprime no terminal o teu primeiro e ultimo nome!
+//  Imprime no console o teu primeiro e ultimo nome!
 
 //  Exemplo: 
 //  Se o nome for "Pedro Leandro Vieira Nascimento", imprime "Pedro Nascimento"
 
 //  ⬇️ Começa na linha 8 ⬇️
 
+console.log("Gabriell Moro");
